02ddf4bffc535d135b95b9953e3231789c33838a
yangshiwen <yangshiwen@xinli001.com>
2020-05-29 13:22:28 +0800

    MybatisPlus 删除集合文件 yangshiwen

--- src/main/java/com/baomidou/mybatisplus/samples/quickstart/mapper/UserMapper.java
+++ src/main/java/com/baomidou/mybatisplus/samples/quickstart/mapper/UserMapper.java
@@ -3,6 +3,9 @@ package com.baomidou.mybatisplus.samples.quickstart.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.samples.quickstart.entity.User;
 
+/**
+ * 用户实体接口
+ */
 public interface UserMapper extends BaseMapper<User> {
 
 }
--- src/test/java/com/baomidou/mybatisplus/samples/quickstart/DemoApplicationTests.java
+++ src/test/java/com/baomidou/mybatisplus/samples/quickstart/DemoApplicationTests.java
@@ -2,14 +2,13 @@ package com.baomidou.mybatisplus.samples.quickstart;
 
 import com.baomidou.mybatisplus.samples.quickstart.entity.User;
 import com.baomidou.mybatisplus.samples.quickstart.mapper.UserMapper;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
-
 
 @SpringBootTest
 public class DemoApplicationTests {
@@ -24,6 +23,19 @@ public class DemoApplicationTests {
         List<User> users = userMapper.selectList(null);
         System.out.println();
         for (User user : users) {
+            System.out.println(user);
+        }
+
+        //ctrl + alt + v 自动补全返回类型
+        System.out.println("删除三个");
+        List ids = new ArrayList();
+        ids.add(1);
+        ids.add(2);
+        ids.add(3);
+        int count = userMapper.deleteBatchIds(ids);
+        List<User> users1 = userMapper.selectList(null);
+        System.out.println(count);
+        for (User user : users1) {
             System.out.println(user);
         }
 
