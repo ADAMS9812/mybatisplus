90fea7ad137b35514e0d7fc6623b00184b173cef
yangshiwen <yangshiwen@xinli001.com>
2020-05-29 15:03:14 +0800

    MybatisPlus 整合CRUD操作 yangshiwen

--- src/main/java/com/baomidou/mybatisplus/samples/quickstart/QuickstartApplication.java
+++ src/main/java/com/baomidou/mybatisplus/samples/quickstart/QuickstartApplication.java
@@ -1,5 +1,6 @@
 package com.baomidou.mybatisplus.samples.quickstart;
 
+import com.baomidou.mybatisplus.samples.quickstart.entity.User;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/baomidou/mybatisplus/samples/quickstart/DemoApplicationTests.java
+++ src/test/java/com/baomidou/mybatisplus/samples/quickstart/DemoApplicationTests.java
@@ -2,7 +2,6 @@ package com.baomidou.mybatisplus.samples.quickstart;
 
 import com.baomidou.mybatisplus.samples.quickstart.entity.User;
 import com.baomidou.mybatisplus.samples.quickstart.mapper.UserMapper;
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -20,6 +19,7 @@ public class DemoApplicationTests {
     public void contextLoads() {
 
         System.out.println("Mybatis-Plus入门案例");
+        //查询所有数据并返回一个集合
         List<User> users = userMapper.selectList(null);
         System.out.println();
         for (User user : users) {
@@ -32,12 +32,86 @@ public class DemoApplicationTests {
         ids.add(1);
         ids.add(2);
         ids.add(3);
+        //删除指定的数据集合
         int count = userMapper.deleteBatchIds(ids);
+        //重新查询数据
         List<User> users1 = userMapper.selectList(null);
         System.out.println(count);
+
         for (User user : users1) {
             System.out.println(user);
         }
+
+    }
+
+    /**
+     * 新增方法
+     */
+    @Test
+    public void insert(){
+
+        //定义一个user
+        User user = new User();
+        user.setId(7);
+        user.setAge(21);
+        user.setName("世文啊啊啊啊啊");
+        user.setEmail("-----yshiwen@qq.com");
+
+        userMapper.insert(user);
+
+    }
+
+    /**
+     * 更新方法
+     */
+    @Test
+    public void update(){
+
+        User user = new User();
+        user.setId(7);
+        user.setAge(222);
+        user.setName("世文测试");
+        user.setEmail("test：yshiwen@qq.com");
+
+        userMapper.updateById(user);
+
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void delete(){
+
+        //数字数组
+        List ids = new ArrayList();
+        ids.add(4);
+        ids.add(5);
+
+        //批量删除
+        int counts = userMapper.deleteBatchIds(ids);
+        System.out.println("集合性删除：" + counts);
+
+        //单独删除
+        int count = userMapper.deleteById(6);
+        System.out.println("单独删除：" + count);
+
+    }
+
+    @Test
+    public void select(){
+
+        //根据id进行查询
+        User user = userMapper.selectById(7);
+        System.out.println(user + "\n");
+
+        //数字集合
+        List list = new ArrayList();
+        list.add(8);
+        list.add(9);
+        //根据id集合进行查询
+        List users = userMapper.selectBatchIds(list);
+        System.out.println(users + "\n");
 
     }
 
